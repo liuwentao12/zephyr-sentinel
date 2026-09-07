ef567d96139393dd5cad2c399e93e5d46c9afcf5
Liu Wentao <142418253+liuwentao12@users.noreply.github.com>
2026-09-07 09:59:02 +0800

    creat project

---
+++ README.md
@@ -0,0 +1,3 @@
+# Zephyr Sentinel
+
+A multi-task embedded monitoring system based on Zephyr RTOS and MAX32655FTHR.
---
+++ src/main.c
@@ -0,0 +1,8 @@
+#include <zephyr/kernel.h>
+#include <zephyr/sys/printk.h>
+
+int main()
+{
+	printk("hello\n");
+	return 0;
+}
